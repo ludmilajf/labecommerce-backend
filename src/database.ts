c1d4b243cd804ef1fd128c02635189729f798663
ludmilajf <ludmiladjf@gmail.com>
2023-01-13 09:36:59 -0300

    Feat Typescript II

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { TProduct, TPurchase, TUser } from "./types";
+import { PRODUCTS_CATEGORY, TProduct, TPurchase, TUser } from "./types";
 
 export const users: TUser[] = [
     {
@@ -23,19 +23,19 @@ export const products: TProduct[] = [
         id: "p001",
         name: "Camiseta",
         price: 70,
-        category: "blusas e casacos"
+        category: PRODUCTS_CATEGORY.CLOTHES
     },
     {
         id: "p002",
         name: "Tenis Nike",
         price: 250,
-        category: "tenis"
+        category: PRODUCTS_CATEGORY.SHOES
     },
     {
         id: "p003",
         name: "Boné NY",
         price: 70,
-        category: "acessorios"
+        category: PRODUCTS_CATEGORY.ACCESSORIES
     }
 ]
 
@@ -54,3 +54,69 @@ export const purchases: TPurchase[] = [
         
     }
 ]
+
+
+
+function createUser (id: string, email: string, password: string){
+    let newUser = {
+        id: id,
+        email: email,
+        password: password
+    } 
+    return users.push(newUser)
+}
+createUser("u004", "ludmila@labenu.com", "ludmila5678")
+
+export function getAllUsers(){
+    return console.table(users)
+}
+
+
+const createProduct = (id: string, name: string, price: number, category: PRODUCTS_CATEGORY) => {
+    const newProduct = {
+        id: id,
+        name: name,
+        price: price,
+        category: category
+    }
+    return products.push(newProduct)
+}
+createProduct("p004","Calça Moletom Nike", 149, PRODUCTS_CATEGORY.CLOTHES)
+
+export const getAllProducts = () => {
+    return console.table(products)
+}
+
+export const getAllProductById = (id: string) => {
+    let idToSearch = products.filter((product) => product.id === id)
+    return console.table(idToSearch)
+}
+
+
+
+export const queryProductsByName = (name: string) => {
+    let q = products.filter((product) => product.name.toLowerCase() === name.toLowerCase())
+    return console.table(q)
+}
+
+
+const createPurchase = (
+    userId: string, 
+    productId: string, 
+    quantity: number, 
+    totalPrice: number
+    ) => {
+    let newPurchase = {
+        userId: userId,
+        productId: productId,
+        quantity: quantity,
+        totalPrice: totalPrice
+    }
+    return purchases.push(newPurchase)
+}
+createPurchase("u002", "p004", 1, 149)
+
+export const getAllPurchasesFromUserId = (userId: string) => {
+    let userIdToSearch = purchases.filter((purchase) => purchase.userId === userId)
+    return console.table(userIdToSearch)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-import { users, products, purchases } from "./database";
+import { users, products, purchases, getAllUsers, getAllProducts, getAllProductById, queryProductsByName, getAllPurchasesFromUserId } from "./database";
 
 console.log(users, products, purchases)
+
+
+getAllUsers()
+getAllProducts()
+getAllProductById("p002")
+queryProductsByName("camiseta")
+getAllPurchasesFromUserId("u001")
