687312a6f71d9ed5166b16eb566d7cba3313553d
ludmilajf <ludmiladjf@gmail.com>
2023-01-17 15:45:54 -0300

    feat feat express

--- src/index.ts
+++ src/index.ts
@@ -25,19 +25,22 @@ app.listen(3003, () => {
 })
 
 
-
+// teste
 app.get('/ping', (req: Request, res: Response) => {
     res.send('Pong!')
 })
 
+//getAllUsers
 app.get('/users', (req: Request, res: Response) => {
     res.status(200).send(users)  
 })
 
+//getAllProducts
 app.get('/products', (req: Request, res: Response) => {
     res.status(200).send(products)
 })
 
+//searchProductsByName
 //esse aqui embaixo não deu certo
 app.get('/product/search', (req: Request, res: Response) => {
     const q = req.query.q as string
@@ -47,6 +50,7 @@ app.get('/product/search', (req: Request, res: Response) => {
     res.status(200).send(result)
 })
 
+//createUser
 app.post('/users', (req: Request, res: Response) => {
     const { id, email, password } = req.body as TUser
     const newUser = {
@@ -58,6 +62,7 @@ app.post('/users', (req: Request, res: Response) => {
     res.status(201).send('Usuário cadastrado com sucesso.')
 })
 
+//createProduct
 app.post('/products', (req: Request, res: Response) => {
     const { id, name, price, category } = req.body as TProduct
     const newProduct = {
@@ -70,6 +75,7 @@ app.post('/products', (req: Request, res: Response) => {
     res.status(201).send("Produto cadastrado com sucesso.")
 })
 
+//createPurchase
 app.post('/purchases', (req: Request, res: Response) => {
     const { userId, productId, quantity, totalPrice } = req.body as TPurchase
     const newPurchase = {
@@ -80,4 +86,73 @@ app.post('/purchases', (req: Request, res: Response) => {
     }
     purchases.push(newPurchase)
     res.status(201).send('Compra realizada com sucesso.')
+})
+
+//getProductsById
+app.get("/products/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+    const result = products.find((product) => product.id === id)
+    res.status(200).send(result)
+})
+
+//getUserPurchasesByUserId
+//também não deu certo
+app.get("/users/:id/purchases", (req: Request, res: Response) => {
+    const userId= req.params.userId
+    const result = purchases.filter((purchase) => purchase.userId === userId)
+    res.status(200).send(result)
+})
+
+//deleteUserById
+app.delete("/users/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+    const userIndex = users.findIndex((user) => user.id === id)
+    if(userIndex >= 0){
+        users.splice(userIndex, 1)
+    }
+    res.status(200).send("Usuário apagado com sucesso.")
+})
+
+//deleteProductById
+app.delete("/products/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+    const productIndex = products.findIndex((product) => product.id === id)
+    if(productIndex >= 0){
+        products.splice(productIndex, 1)
+    }
+    res.status(200).send("Produto apagado com sucesso.")
+})
+
+//editUserById
+app.put("/users/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+    const { email, password } = req.body as TUser
+    const editUser = {
+        email,
+        password
+    }
+    const user = users.find((user) => user.id === id)
+    if(user) {
+        user.email = editUser.email || user.email
+        user.password = editUser.password || user.password
+    }
+    res.status(200).send("Cadastro atualizado com sucesso.")
+})
+
+//editProductById
+app.put("/products/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+    const { name, price, category } = req.body as TProduct
+    const editProduct = {
+        name,
+        price,
+        category
+    }
+    const product = products.find((product) => product.id === id)
+    if(product) {
+        product.name = editProduct.name || product.name
+        product.price = editProduct.price || product.price
+        product.category = editProduct.category || product.category
+    }
+    res.status(200).send("Produto atualizado com sucesso.")
 })
